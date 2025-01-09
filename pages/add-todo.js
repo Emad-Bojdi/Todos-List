a100df3dcd6c2bb6b9a8bad4d64d0106cb27817b
Emaad <emadfredev@gmail.com>
2025-01-09 12:22:37 +0330

    Edited add-todo file

--- pages/add-todo.js
+++ pages/add-todo.js
@@ -2,10 +2,11 @@ import React from 'react'
 import AddTodo from "../components/templates/AddTodo"
 import { getSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import { authOptions } from './api/auth/[...nextauth]';
 
 function addtodo() {
 
-  const status = getSession();
+  const status = getSession(authOptions);
   const router = useRouter();
   if (status === "unauthenticated")
     router.push("/signin");
